d0782e719b3072a466494178967ef416edb3a64d
Lukas Mauser <l.mauser@wimadev.de>
2024-06-21 10:00:04 +0200

    feat: https redirs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-# Replace the placeholder with the environment variable value
-envsubst '$TARGET_DOMAIN' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+# Set default value for HTTPS_REDIRECT if not provided
+: "${HTTPS_REDIRECT:=false}"
+
+# Export variables to be used by envsubst
+export TARGET_DOMAIN
+export HTTPS_REDIRECT
+
+# Replace the placeholders with the environment variable values
+envsubst '$TARGET_DOMAIN $HTTPS_REDIRECT' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
